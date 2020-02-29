858fa889b100ed084c8a86d51a9c87286a948b68
Tianhua Zhao <zhaot2@seattleu.edu>
2020-02-29 00:24:51 -0800

    Updated Page URLs

--- AddressBook/Startup.cs
+++ AddressBook/Startup.cs
@@ -29,7 +29,6 @@ namespace AddressBook
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<AddressService>();
         }
 
