f0fa425762694c9ceeed6411f51628ec8c5ad76b
Tianhua Zhao <zhaot2@seattleu.edu>
2020-03-03 01:14:15 -0800

    Added Annotations

--- AddressBook/Controllers/ApiController.cs
+++ AddressBook/Controllers/ApiController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AddressBook.Controllers
 {
+    [Produces("application/json")]
     [Route("api")]
     [ApiController]
     public class ApiController : ControllerBase
@@ -15,16 +16,18 @@ namespace AddressBook.Controllers
         AddressService _addressService = new AddressService();
 
         [HttpGet("index")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<List<Address>> Get() =>
             
             _addressService.GetAllAddresses();
 
         [HttpPost("add")]
-        public ActionResult<Address> Create(Address address)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Address> Create([FromBody] Address address)
         {
             _addressService.CreateAddress(address);
-
-            return NoContent();
+            return address;
         }
     }
 }
--- AddressBook/Data/Address.cs
+++ AddressBook/Data/Address.cs
@@ -1,5 +1,5 @@
-﻿
-
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
@@ -14,6 +14,7 @@ namespace AddressBook.Data
 
         // Country name.
         [BsonElement("country")]
+        [Required]
         public string Country { get; set; }
 
         // The address lines represent the most specific part of any address.
