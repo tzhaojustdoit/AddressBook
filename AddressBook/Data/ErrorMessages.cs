5e8cd5da913db56f1130457cda135c05cfca7f8c
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-03-06 15:41:35 -0800

    Added ErrorMessages

---
+++ AddressBook/Data/ErrorMessages.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Data
+{
+    
+    public class EmptyCountryErrror
+    {
+        public int ErrorCode { get => 100; }
+
+        public string Message { get => "Country input is empty"; }
+    }
+
+    public class EmptyAddrLineErrror
+    {
+        public int ErrorCode { get => 100; }
+
+        public string Message { get => "Address Line 1 is empty"; }
+    }
+
+    public class EmptyAdminAreaErrror
+    {
+        public int ErrorCode { get => 100; }
+
+        public string admin { get; set; }
+
+        public string Message { get => admin + " is empty"; }
+    }
+
+}
